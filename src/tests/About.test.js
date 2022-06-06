97291d203521ac6a1bc9e37c165c09020ad13802
Kevin Li <Kevinstart14@gmail.com>
2022-06-06 20:20:34 -0300

    erro lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,12 +6,17 @@ import renderWithRouter from '../components/renderWithRouter';
 describe('2. Teste o componente <About.js />.', () => {
   test('se a página contém um heading h2 com o texto About Pokédex', () => {
     renderWithRouter(<About />);
-    const head = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    const head = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
     expect(head).toBeInTheDocument();
   });
   test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
     renderWithRouter(<About />);
-    const paragrafo1 = screen.getByText(/This application simulates a Pokédex/i);
+    const paragrafo1 = screen.getByText(
+      /This application simulates a Pokédex/i,
+    );
     const paragrafo2 = screen.getByText(/One can filter Pokémons by type/i);
     expect(paragrafo1).toBeInTheDocument();
     expect(paragrafo2).toBeInTheDocument();
@@ -19,7 +24,9 @@ describe('2. Teste o componente <About.js />.', () => {
   test('se a página contém a imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
     const imagem = screen.getByRole('img');
-    expect(imagem).toHaveProperty('src',
-      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(imagem).toHaveProperty(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 
 describe('4. Teste o componente <NotFound.js />', () => {
-  test(
-    'Teste se a página contém um heading h2 com o texto Page requested not found 😭',
-    () => {
-      const { history } = renderWithRouter(<App />);
-      history.push('/naoexiste');
-      const notFoundText = screen.getByText(
-        /Page requested not found/i,
-        { level: 2 },
-      );
-      expect(notFoundText).toBeInTheDocument();
-    },
-  );
+  test('Teste se contém um heading h2 com o texto Page requested not found ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/naoexiste');
+    const notFoundText = screen.getByText(/Page requested not found/i, {
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
   test('Teste se a página mostra a imagem', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/naoexiste');
     const image = screen.getAllByRole('img');
-    expect(image[1]).toHaveProperty('src',
-      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image[1]).toHaveProperty(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
   });
 });
