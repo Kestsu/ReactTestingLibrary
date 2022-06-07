9009217ed6974c8ce1f92a27ecf3bcdff6d143ae
Kevin Li <Kevinstart14@gmail.com>
2022-06-07 15:02:16 -0300

    segundo dia

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,74 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('5. Teste o componente <Pokedex.js />', () => {
+  test('Teste se contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const titulo = screen.getByRole(
+      'heading', {
+        name: /Encountered pokémons/i, level: 2,
+      },
+    );
+    expect(titulo).toBeInTheDocument();
+  });
+  test('Teste se é exibido o próximo pokémon da lista quando o botão é clicado', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getByTestId('next-pokemon');
+    expect(botao).toBeInTheDocument();
+    userEvent.click(botao);
+    const charmander = screen.getByText(/Charmander/i);
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(botao);
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
+    userEvent.click(botao);
+    const ekans = screen.getByText(/Ekans/i);
+    expect(ekans).toBeInTheDocument();
+    userEvent.click(botao);
+    const alakazam = screen.getByText(/Alakazam/i);
+    expect(alakazam).toBeInTheDocument();
+    userEvent.click(botao);
+    const mew = screen.getByText(/mew/i);
+    expect(mew).toBeInTheDocument();
+    userEvent.click(botao);
+    const rapidash = screen.getByText(/Rapidash/i);
+    expect(rapidash).toBeInTheDocument();
+    userEvent.click(botao);
+    const snorlax = screen.getByText(/Snorlax/i);
+    expect(snorlax).toBeInTheDocument();
+    userEvent.click(botao);
+    const dragon = screen.getByText(/Dragonair/i);
+    expect(dragon).toBeInTheDocument();
+    userEvent.click(botao);
+    const pikachu = screen.getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+  //     test('Teste se é mostrado apenas um pokémon por vez', () => {
+  // renderWithRouter(<App />);
+
+  //     });
+  test('Teste se a Pokédex tem os botões de filtro:', () => {
+    renderWithRouter(<App />);
+    const all = screen.getByRole('button', { name: /All/i });
+    const electric = screen.getByRole('button', { name: /Electric/i });
+    const fire = screen.getByRole('button', { name: /Fire/i });
+    const bug = screen.getByRole('button', { name: /Bug/i });
+    const poison = screen.getByRole('button', { name: /Poison/i });
+    const psychic = screen.getByRole('button', { name: /Psychic/i });
+    const normal = screen.getByRole('button', { name: /Normal/i });
+    const dragon = screen.getByRole('button', { name: /Dragon/i });
+    expect(all).toBeInTheDocument();
+    expect(electric).toBeInTheDocument();
+    expect(fire).toBeInTheDocument();
+    expect(bug).toBeInTheDocument();
+    expect(poison).toBeInTheDocument();
+    expect(psychic).toBeInTheDocument();
+    expect(normal).toBeInTheDocument();
+    expect(dragon).toBeInTheDocument();
+  });
+//   test('', () => {});
+//   test('', () => {});
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,65 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('7. Teste o componente <PokemonDetails.js />', () => {
+  test('Teste se as informações detalhadas são mostradas na tela:', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getByText(/More details/i);
+    userEvent.click(botao);
+
+    const titulo = screen.getByText(/Pikachu Details/i);
+    expect(titulo).toBeInTheDocument();
+
+    const heading = screen.getByRole('heading', { name: /Summary/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+
+    const detalhes = screen.getByText(
+      /This intelligent Pokémon roasts hard berries with electricity/i,
+    );
+    expect(detalhes).toBeInTheDocument();
+  });
+  test('Teste se existe mapas contendo as localizações do pokémon', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getByText(/More details/i);
+    userEvent.click(botao);
+
+    const heading = screen.getByRole(
+      'heading', {
+        name: /Game Locations of Pikachu/i, level: 2,
+      },
+    );
+    expect(heading).toBeInTheDocument();
+
+    const mapaUm = screen.getByText(/Kanto Viridian Forest/i);
+    const mapaDois = screen.getByText(/Kanto Power Plant/i);
+    expect(mapaUm).toBeInTheDocument();
+    expect(mapaDois).toBeInTheDocument();
+
+    const image = screen.getAllByRole('img');
+    expect(image[1]).toHaveProperty(
+      'src',
+      'https://pwo-wiki.info/images/4/47/Viridian_Forest.gif',
+    //   'alt',
+    //   'Pikachu location',
+    );
+    expect(image[2]).toHaveProperty(
+      'src',
+      'https://pwo-wiki.info/images/5/5b/Pp.gif',
+    //   'alt',
+    //   'Pikachu location',
+    );
+  });
+  test('Teste se pode favoritar um pokémon através da página de detalhes', () => {
+    renderWithRouter(<App />);
+    const botao = screen.getByText(/More details/i);
+    userEvent.click(botao);
+
+    const check = screen.getByRole('checkbox', {
+      name: /pokémon favoritado\?/i,
+    });
+    expect(check).toBeInTheDocument();
+  });
+});
